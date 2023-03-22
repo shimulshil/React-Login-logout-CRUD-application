2a0ce2622aa2e7852d10e5ca35b7473f479651ce
shimul shil <72187771+shimulshil@users.noreply.github.com>
2023-03-23 00:21:29 +0100

    Update connection.js
    Azure database in Mysql

--- Server/connection.js
+++ Server/connection.js
@@ -7,6 +7,27 @@ var connection = mysql.createConnection({
     database:"crud"
 })
 
+//--------------------------------------------------------------------------
+// Database connection with Azure Database for Mysql 
+// need to download the SSL file from azure mysql server and save in your c: drive (Ex-c:/var/www/html/BaltimoreCyberTrustRoot.crt.pem)
+// https://learn.microsoft.com/en-us/azure/mysql/single-server/connect-nodejs
+
+// var fs = require('fs');
+// var mysql = require('mysql');
+// const serverCa = [fs.readFileSync("/var/www/html/BaltimoreCyberTrustRoot.crt.pem", "utf8")];
+// var connection=mysql.createConnection({
+//     host:"az-900-sql-server.mysql.database.azure.com",
+//     user:"shimul",
+//     password:"Aradho21",
+//     database:"crud",
+//     port:3306,
+//     ssl: {
+//         rejectUnauthorized: true,
+//         ca: serverCa
+//     }
+// })
+//-------------------------------------------------------------------------
+
 connection.connect((err)=>{
     if(!err){
         console.log(' & connected successfully');
